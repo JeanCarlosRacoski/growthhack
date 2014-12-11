292a2486959c66ca232004bfc7588432f53811d8
JeanCarlosRacoski <jean.c.racoski@gmail.com>
2014-12-11 00:14:30 +0000

    Sending folders

---
+++ www/index.php
@@ -0,0 +1,138 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <!-- Place this data between the <head> tags of your website -->
+    <title>Growth Hack: Programa de Growth Hacking para Startups</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="description" content="Multiplique o seu nÃºmero de usuÃ¡rios e clientes nesse programa de 8 semanas, utilizando tÃ©cnicas profissionais de Growth Hacking." />
+
+    <!-- Google Authorship and Publisher Markup -->
+    <link rel="author" href="https://plus.google.com/u/0/113153333413583477752/posts"/>
+    <link rel="publisher" href="https://plus.google.com/u/0/113153333413583477752/about"/>
+
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="Programa de Growth Hacking para Startups">
+    <meta itemprop="description" content="Multiplique o seu nÃºmero de usuÃ¡rios e clientes nesse programa de 8 semanas, utilizando tÃ©cnicas profissionais de Growth Hacking.">
+    <meta itemprop="image" content="http://www.growthhack.com.br/background-facebook.jpg">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="Programa de Growth Hacking para Startups em 8 semanas: http://www.growthhack.com.br">
+    <meta name="twitter:site" content="@ramonbez">
+    <meta name="twitter:title" content="Programa de Growth Hacking">
+    <meta name="twitter:description" content="Multiplique o seu nÃºmero de usuÃ¡rios e clientes nesse programa de 8 semanas, utilizando tÃ©cnicas profissionais de Growth Hacking.">
+    <meta name="twitter:creator" content="@ramonbez">
+    <!-- Twitter summary card with large image must be at least 280x150px -->
+    <meta name="twitter:image:src" content="http://www.growthhack.com.br/background-facebook.jpg">
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Programa de Growth Hacking para Startups" />
+    <meta property="og:type" content="" />
+    <meta property="og:url" content="http://www.growthhack.com.br/" />
+    <meta property="og:image" content="http://www.growthhack.com.br/background-facebook.jpg" />
+    <meta property="og:description" content="Multiplique o seu nÃºmero de usuÃ¡rios e clientes nesse programa de 8 semanas, utilizando tÃ©cnicas profissionais de Growth Hacking." />
+    <meta property="og:site_name" content="Growth Hack" />
+
+    <link rel="icon" href="../../favicon.ico">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="css/growth.css" rel="stylesheet">
+
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+
+        <div class="top-border">
+        </div>
+        <div class="container">
+          <div class="container-growth">
+            <a href="http://www.growthhack.com.br/growh-hack-dashboard.html"><img src="http://www.growthhack.com.br/logo-growth-blue.png" width="170px"></a>
+            <div style="float: right">
+              <a href="#">Logout</a>
+            </div>
+          <hr>
+           <div class="col-md-9">
+            <div class="main-article">
+            <h2>Bem vindo ao <span class="text-muted">Curso de Growth Hacking</span></h2>
+            <p class="lead">
+              Ã€ direita da tela vocÃª tem acesso aos conteÃºdos das liÃ§Ãµes. Clique na liÃ§Ã£o correspondente ao seu estÃ¡gio no curso e boa sorte!
+            </p>
+            <p>
+              Para dÃºvidas ou maiores informaÃ§Ãµes, por favor escreva para <a href="mailto:ramon@growthhack.com.br">ramon@growthhack.com.br</a>.
+            </p>
+            </div><!-- .main-article -->
+          </div><!-- .col-md-9 -->
+
+          <div class="col-md-3">
+            <div class="outline-header">
+             CONTEÃšDO DO CURSO:
+            </div>
+            <a href="/page.php"><strong>LiÃ§Ã£o 1:</strong> Conhecendo seu usuario melhor do que ele mesmo.</a><br><br>
+            <a href="http://www.growthhack.com.br/growth-lesson2.html"><strong>LiÃ§Ã£o 2:</strong> Criando uma empresa voltada para o growth (parte 1).</a><br><br>
+            <a href="http://www.growthhack.com.br/growth-lesson3.html"><strong>LiÃ§Ã£o 3:</strong> Criando uma empresa voltada para o growth (parte 2).</a><br><br>
+            <a href="http://www.growthhack.com.br/growth-lesson4.html"><strong>LiÃ§Ã£o 4:</strong> RetenÃ§Ã£o - Criando laÃ§os duradouros entre usuÃ¡rio e produto.</a><br><br>
+            <a href="http://www.growthhack.com.br/growth-lesson5.html"><strong>LiÃ§Ã£o 5:</strong> AquisiÃ§Ã£o - Atraindo os clientes certos (parte 1).</a><br><br>
+            <a href="http://www.growthhack.com.br/growth-lesson6.html"><strong>LiÃ§Ã£o 6:</strong> AquisiÃ§Ã£o - Atraindo os clientes certos (parte 2).</a>
+          </div><!-- .col-md-3 -->
+
+        </div><!-- .container-growth -->
+        </div><!-- .container -->
+  
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-55539410-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
+    <!-- AdRoll -->
+    <script type="text/javascript">
+      adroll_adv_id = "UJ2HLI2OLFGL3NHS5H2M2X";
+      adroll_pix_id = "RYDBLP2T7VGFJAN6CSRIZF";
+      (function () {
+      var oldonload = window.onload;
+      window.onload = function(){
+         __adroll_loaded=true;
+         var scr = document.createElement("script");
+         var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+         scr.setAttribute('async', 'true');
+         scr.type = "text/javascript";
+         scr.src = host + "/j/roundtrip.js";
+         ((document.getElementsByTagName('head') || [null])[0] ||
+          document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+         if(oldonload){oldonload()}};
+      }());
+    </script>
+
+  </body>
+</html>
---
+++ www/page.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Isto é um < p > que foi execitado de dentro de um arquivo php</p>'; ?> 
+ </body>
+</html>
